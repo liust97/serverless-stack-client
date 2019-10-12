3267a86505cb536b80b5630917a12139e0c10d3d
Sitong Liu <liusit@foxmail.com>
2019-10-12 12:08:17 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "huscii-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://7ji28ya0929.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "YOUR_COGNITO_REGION",
+      USER_POOL_ID: "us-east-1_L10bHPFWW",
+      APP_CLIENT_ID: "YOUR_CO2socgj3ch9j9mdnav23nqasi0fGNITO_APP_CLIENT_ID",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+  };2
